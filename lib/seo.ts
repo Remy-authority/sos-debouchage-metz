11a34b6a5ada6ae37142e802dc19a2325819f1ae
Remy-authority <remy@remyzaoui.com>
2026-08-26 02:24:31 +0700

    SEO : retire priceRange du JSON-LD (donnée inventée, arbitrage Rémy 26/08)

--- lib/seo.ts
+++ lib/seo.ts
@@ -99,7 +99,6 @@ export function localBusinessJsonLd() {
     telephone: siteConfig.phone,
     email: siteConfig.email,
     image: absUrl(siteConfig.seo.defaultOgImage),
-    priceRange: '€€',
     areaServed: areaServed.map((name) => ({ '@type': 'City', name })),
     openingHoursSpecification: [
       {
